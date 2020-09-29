280fbc4c180ada6bfeb4d902f2d4e76bee0be5fd
Dennis Verspuij <dverspuij@cnsconnect.nl>
2020-09-29 11:00:41 +0200

    Remove orphaned model classes

--- Model/GsMedischeHulpmiddelenQuery.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PharmaIntelligence\GstandaardBundle\Model;
-
-use PharmaIntelligence\GstandaardBundle\Model\om\BaseGsMedischeHulpmiddelenQuery;
-
-class GsMedischeHulpmiddelenQuery extends BaseGsMedischeHulpmiddelenQuery
-{
-}
