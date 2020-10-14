fc7996cd46177bb683619a1bd685969b3d346e9a
Dennis Verspuij <dverspuij@cnsconnect.nl>
2020-10-14 11:42:59 +0200

    Removed obtrusive GsBestanden::getUitgavedatum() override, assuming propel.useDateTimeClass is disabled. With propel.useDateTimeClass false and propel.defaultDateFormat 'd-m-Y' similar default response can be configured.

--- Model/GsBestanden.php
+++ Model/GsBestanden.php
@@ -6,10 +6,4 @@ use PharmaIntelligence\GstandaardBundle\Model\om\BaseGsBestanden;
 
 class GsBestanden extends BaseGsBestanden
 {
-	public function getUitgavedatum($format = 'd-m-Y') {
-		$uitgaveDatum = parent::getUitgavedatum();
-		if(strlen(parent::getUitgavedatum()) == 7)
-			$uitgaveDatum = "0".parent::getUitgavedatum();
-		return \DateTime::createFromFormat('dmY', $uitgaveDatum)->format($format);
-	}
 }
